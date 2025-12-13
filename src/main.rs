0fdf1235a2b41ebf6d1d0194453e9a7a19f67b12
Bas van der Hoek <bas@hoek.com>
2025-12-13 18:06:43 +0100

    Add tags

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #![allow(non_snake_case)]
 use dotenvy::dotenv;
-use sqlx::{sqlite::SqliteConnectOptions, ConnectOptions};
+use sqlx::{sqlite::SqliteConnectOptions, ConnectOptions, Row};
 use std::env;
 use std::str::FromStr;
+use uuid::Uuid;
 
 #[derive(Debug, sqlx::FromRow)]
 #[allow(unused)]
@@ -28,10 +29,35 @@ async fn connect() -> anyhow::Result<()> {
       .pragma("key", key)
       .connect().await?;
 
-  let row = sqlx::query_as::<_, Content>("SELECT * FROM djmdContent WHERE FileNameL like ?")
+  let content = sqlx::query_as::<_, Content>("SELECT * FROM djmdContent WHERE FileNameL like ?")
     .bind(format!("%[{}]%", "918205852"))
     .fetch_one(&mut conn).await?;
+  println!("{:?}", content);
 
-  println!("{:?}", row);
+  sqlx::query("UPDATE agentRegistry SET int_1 = int_1 + 1 WHERE registry_id = 'localUpdateCount'")
+    .execute(&mut conn).await?;
+  let usn = sqlx::query("SELECT int_1 from agentRegistry WHERE registry_id = 'localUpdateCount'")
+    .fetch_one(&mut conn).await?;
+  let next_usn: i64 = usn.try_get("int_1")?;
+  println!("next usn {}", next_usn);
+  // let next_usn = sqlx::query_as::<_, String>("SELECT int_1 from agentRegistry WHERE registry_id = 'localUpdateCount'").fetch_one(&mut conn).await?;
+
+  let insert = r#"
+      WITH
+        tag AS (SELECT ID, ParentID FROM djmdMyTag WHERE name = ?)
+      INSERT INTO djmdSongMyTag (ID, MyTagID, ContentID, UUID, rb_local_usn, created_at, updated_at)
+        SELECT ?, tag.ID, ?, ?, ?, datetime(), datetime()
+        FROM tag
+      WHERE NOT EXISTS(SELECT * FROM djmdSongMyTag WHERE MyTagID = tag.ID AND ContentID = ?)
+  "#;
+  let tags = sqlx::query(insert)
+    .bind("edrive")
+    .bind(Uuid::new_v4().to_string())
+    .bind(&content.ID)
+    .bind(Uuid::new_v4().to_string())
+    .bind(next_usn)
+    .bind(&content.ID)
+    .execute(&mut conn).await?;
+
   Ok(())
 }
