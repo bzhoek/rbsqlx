29cc0977eb0c654fd37ea633189639c9e5235eb5
Bas van der Hoek <bas@hoek.com>
2025-12-13 18:41:12 +0100

    Only generate usn when tag does not exist

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case)]
 use dotenvy::dotenv;
-use sqlx::{sqlite::SqliteConnectOptions, ConnectOptions, Row};
+use sqlx::{sqlite::SqliteConnectOptions, ConnectOptions, Row, SqliteConnection};
 use std::env;
 use std::str::FromStr;
 use uuid::Uuid;
@@ -34,30 +34,55 @@ async fn connect() -> anyhow::Result<()> {
     .fetch_one(&mut conn).await?;
   println!("{:?}", content);
 
-  sqlx::query("UPDATE agentRegistry SET int_1 = int_1 + 1 WHERE registry_id = 'localUpdateCount'")
-    .execute(&mut conn).await?;
-  let usn = sqlx::query("SELECT int_1 from agentRegistry WHERE registry_id = 'localUpdateCount'")
-    .fetch_one(&mut conn).await?;
-  let next_usn: i64 = usn.try_get("int_1")?;
-  println!("next usn {}", next_usn);
-  // let next_usn = sqlx::query_as::<_, String>("SELECT int_1 from agentRegistry WHERE registry_id = 'localUpdateCount'").fetch_one(&mut conn).await?;
+  let exists = tag_exists(&mut conn, &content, "vocals").await?;
+  if !exists {
+    increment_usn(&mut conn).await?;
+    let next_usn = next_usn(&mut conn).await?;
+    println!("next usn {}", next_usn);
+    insert_tag(&mut conn, &content, next_usn, "vocals").await?;
+  }
 
+  Ok(())
+}
+
+async fn insert_tag(conn: &mut SqliteConnection, content: &Content, next_usn: i64, name: &str) -> anyhow::Result<()> {
   let insert = r#"
       WITH
         tag AS (SELECT ID, ParentID FROM djmdMyTag WHERE name = ?)
       INSERT INTO djmdSongMyTag (ID, MyTagID, ContentID, UUID, rb_local_usn, created_at, updated_at)
         SELECT ?, tag.ID, ?, ?, ?, datetime(), datetime()
         FROM tag
-      WHERE NOT EXISTS(SELECT * FROM djmdSongMyTag WHERE MyTagID = tag.ID AND ContentID = ?)
   "#;
-  let tags = sqlx::query(insert)
-    .bind("edrive")
+  sqlx::query(insert)
+    .bind(name)
     .bind(Uuid::new_v4().to_string())
     .bind(&content.ID)
     .bind(Uuid::new_v4().to_string())
     .bind(next_usn)
-    .bind(&content.ID)
-    .execute(&mut conn).await?;
+    .execute(conn).await?;
+  Ok(())
+}
 
+async fn tag_exists(conn: &mut SqliteConnection, content: &Content, name: &str) -> anyhow::Result<bool> {
+  let exists = r#"
+      SELECT EXISTS(SELECT * FROM djmdSongMyTag AS st, djmdMyTag as t WHERE st.MyTagID = t.ID AND t.Name = ? AND ContentID = ?)
+  "#;
+  let exists: bool = sqlx::query_scalar(exists)
+    .bind(name)
+    .bind(&content.ID)
+    .fetch_one(conn).await?;
+  Ok(exists)
+}
+
+async fn next_usn(conn: &mut SqliteConnection) -> anyhow::Result<i64> {
+  let usn = sqlx::query("SELECT int_1 from agentRegistry WHERE registry_id = 'localUpdateCount'")
+    .fetch_one(conn).await?;
+  let next_usn: i64 = usn.try_get("int_1")?;
+  Ok(next_usn)
+}
+
+async fn increment_usn(conn: &mut SqliteConnection) -> anyhow::Result<()> {
+  sqlx::query("UPDATE agentRegistry SET int_1 = int_1 + 1 WHERE registry_id = 'localUpdateCount'")
+    .execute(conn).await?;
   Ok(())
 }
