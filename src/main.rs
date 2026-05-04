06c8badd6f2b9680b5fbd7cf86d9d371ce4f410a
Bas van der Hoek <bas@hoek.com>
2026-05-04 10:54:54 +0200

    Remove unnecessary mut

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use rbsqlx::Database;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-  let mut db = Database::connect("encrypted.db").await?;
+  let db = Database::connect("encrypted.db").await?;
   let content = db.content("918205852").await?;
   println!("{:?}", content);
   db.rate_content(&content, 4).await?;
